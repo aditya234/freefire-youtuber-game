b969361f852c57f43992cf21b9ee80ad0db9b906
Aditya Soni <adityasoni5859@gmail.com>
2019-12-07 22:20:12 +0530

    code refactor

---
+++ lib/screens/youtuber_details/index.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class YoutuberDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
