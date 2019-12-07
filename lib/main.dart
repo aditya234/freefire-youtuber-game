ff9b11d6ee612282ac7589415214ca736f74e4d1
Aditya Soni <adityasoni5859@gmail.com>
2019-12-07 19:04:57 +0530

    added google services json file to connect to firebase from Android end

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_freefire/screens/index.dart';
+import 'package:flutter_freefire/screens/auth/index.dart';
 
 void main() => runApp(MyApp());
 
